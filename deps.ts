7a8214d2f3d6ec216b8690a793f03faba9656bb5
Shahid N. Shah <shahid@shah.org>
2020-10-04 17:45:17 -0400

    fix: upgrade to deno 1.4.4 and dependencies

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * as fs from "https://deno.land/std@v0.70.0/fs/mod.ts";
-export * as path from "https://deno.land/std@v0.70.0/path/mod.ts";
+export * as fs from "https://deno.land/std@v0.73.0/fs/mod.ts";
+export * as path from "https://deno.land/std@v0.73.0/path/mod.ts";
