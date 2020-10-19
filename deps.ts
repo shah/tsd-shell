9026e4f50e197932b5bccd42238e7c8175e1fde9
Shahid N. Shah <shahid@shah.org>
2020-10-19 09:42:07 -0400

    fix: remove Deno std@v from dependencies

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * as fs from "https://deno.land/std@v0.73.0/fs/mod.ts";
-export * as path from "https://deno.land/std@v0.73.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.74.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.74.0/path/mod.ts";
